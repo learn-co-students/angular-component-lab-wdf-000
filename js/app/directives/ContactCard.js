6127b7f24735ca9dbc70ec483af382236776ade3
lastromanticx <giladbarkan@live.com>
2016-08-08 10:16:03 -0400

    Done.

--- js/app/directives/ContactCard.js
+++ js/app/directives/ContactCard.js
@@ -1,23 +1,23 @@
 var ContactCard = {
-		bindings: {
-			name: '=',
-			email: '=',
-			phone: '='
-		},
-		template: [
-			'<div>',
-				'<h4>Contact Card</h4>',
-				'<label>Name:</label>',
-				'{{ ctrl.name }}',
-				'<label>Email:</label>',
-				'{{ ctrl.email }}',
-				'<label>Phone:</label>',
-				'{{ ctrl.phone }}',
-			'</div>'
-		].join(''),
-		restrict: 'E',
-    controllerAs: 'ctrl'
-	};
+  bindings: {
+    name: '=',
+    email: '=',
+    phone: '='
+  },
+  template: [
+    '<div>',
+      '<h4>Contact Card</h4>',
+      '<label>Name:</label>',
+      '{{ ctrl.name }}',
+      '<label>Email:</label>',
+      '{{ ctrl.email }}',
+      '<label>Phone:</label>',
+      '{{ ctrl.phone }}',
+    '</div>'
+  ].join(''),
+  restrict: 'E',
+  controllerAs: 'ctrl'
+};
 
 angular
 	.module('app')
